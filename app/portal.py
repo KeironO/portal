bfade3301ddbb3710972e772197e6119aaae2b52
Keiron O'Shea <keiron.oshea@gmail.com>
2018-08-23 15:00:23 +0100

    Need to add bower stuff

--- app/portal.py
+++ app/portal.py
@@ -1,5 +1,16 @@
 import os
 from flask import Flask
 
+app = Flask(__name__)
 
+@app.route("/help")
+def help():
+    return "Help page goes here."
 
+@app.route("/classifier")
+def classifier():
+    return None
+
+@app.route("/")
+def main():
+    return "Hello World"
