c67e783d5d5369601539f90552d94c15c58efce3
Keiron O'Shea <keiron.oshea@gmail.com>
2018-08-23 23:22:47 +0100

    Whoops

--- app/portal.py
+++ app/portal.py
@@ -3,19 +3,19 @@ from flask import Flask
 from flask_bower import Bower
 from flask import render_template
 
+import utils
 
 app = Flask(__name__)
 Bower(app)
 
-
-@app.route("/help")
-def help():
+@app.route("/docs")
+def docs():
     return "Help page goes here."
 
-@app.route("/classifier")
-def classifier():
-    return None
+@app.route("/classifiers")
+def classifiers():
+    return render_template("classifier.html")
 
 @app.route("/")
-def main():
+def index():
     return render_template("index.html")
