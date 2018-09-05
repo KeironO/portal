11046676b930943232eb879b1fef81edd2140ca7
KeironO <keiron.oshea@gmail.com>
2018-09-05 14:50:05 +0100

    Looking good

--- app/static/js/results.js
+++ app/static/js/results.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     // Populate results table, alpha.
     $.getJSON(get_url, function(data) {
         var predictions = data["_items"];
-        chart(predictions, 3);
+        chart(predictions, 0);
         $("#results-table").DataTable({
             "data": predictions,
             "pageLength": 10,
@@ -32,45 +32,59 @@ $(document).ready(function() {
         });
     });
 
+    function cleanPredictions(predictions) {
+        var cleanPredictions = { }
 
-    function chart(predictions, index) {
-        var counts = {};
         predictions.forEach(function(sequence) {
-            var pred = sequence["predictions"][index];
-            if (pred[0] in counts) {
-                counts[pred[0]] += 1
-            } else {
-                counts[pred[0]] = 1
-            }
-        });
-        var bcs = palette('tol', Object.keys(counts).length).map(function(hex) {
-              return '#' + hex;
-            });
-
-        console.log(bcs, typeof bcs, Object.values(bcs));
-
-        var ctx = document.getElementById("myChart");
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: Object.keys(counts),
-                datasets: [{
-                    label: '# of Annotations',
-                    data: Object.values(counts),
-                    backgroundColor: Object.values(bcs)
-                }],
-
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
+            var preds = sequence["predictions"];
+            var cleanedPreds = []
+            preds.forEach(function(p) {
+                if (p[0] != "null") {
+                  cleanedPreds.push(p[0]);
                 }
+              });
+            cleanPredictions[sequence["seq_id"]] = cleanedPreds.join("; ");
+        });
+
+        return Object.values(cleanPredictions);
+
+    }
+
+
+    function chart(predictions) {
+        var clearedPredictions = cleanPredictions(predictions);
+        var counts = {};
+        clearedPredictions.forEach(function(sequence) {
+            if (sequence in counts) {
+                counts[sequence] += 1
+            }
+
+            else {
+                counts[sequence] = 1
             }
         });
+
+        var data = [{
+        type: "bar",
+        y: Object.values(counts),
+        x: Object.keys(counts),
+        orientation: "v"
+      }];
+
+      var layout = {
+      xaxis: {
+
+    autotick: true,
+    autorange: true,
+    showticklabels: false
+  }
+      }
+
+      Plotly.newPlot("results-plot", data, layout)
+
+
+
     }
+
 
 });
