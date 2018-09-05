f22841734ae1afa1bee5c9198381def146936f59
KeironO <keiron.oshea@gmail.com>
2018-09-05 12:46:57 +0100

    Hmm

--- app/static/js/results.js
+++ app/static/js/results.js
@@ -1,31 +1,75 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip()
     var get_url = window.location.href + "/get";
-    $("#example").DataTable({
-        "ajax": {
-            "url" : get_url,
-            "dataSrc" : "_items"
-        },
-        "pageLength" : 50,
-        "columns" : [
-            {
-                "title": "Sequence ID",
-                "data" : "seq_id"
-            },
-            {
-                "title": "Results",
-                "data" : "predictions",
-                "render" : function(data, type, row) {
-                    var final_str = ""
-                    for (i in data) {
-                        var prediction = data[i];
-                        if (prediction[0] != "null") {
-                            final_str += '<span data-toggle="tooltip" data-placement="top" title="Confidence Value: ' + prediction[1]*100 + '%">' + prediction[0] + '; </span>'
-                        } 
+
+
+    // Populate results table, alpha.
+    $.getJSON(get_url, function(data) {
+        var predictions = data["_items"];
+        chart(predictions, 4);
+        $("#results-table").DataTable({
+            "data": predictions,
+            "pageLength": 10,
+            "columns": [{
+                    "title": "Sequence ID",
+                    "data": "seq_id"
+                },
+                {
+                    "title": "Results",
+                    "data": "predictions",
+                    "render": function(data, type, row) {
+                        var final_str = ""
+                        for (i in data) {
+                            var prediction = data[i];
+                            if (prediction[0] != "null") {
+                                final_str += '<span data-toggle="tooltip" data-placement="top" title="Confidence Value: ' + prediction[1] * 100 + '%">' + prediction[0] + '; </span>'
+                            }
+                        }
+                        return final_str
                     }
-                    return final_str
+                }
+            ]
+        });
+    });
+
+
+    function chart(predictions, index) {
+        var counts = {};
+        predictions.forEach(function(sequence) {
+            var pred = sequence["predictions"][index];
+            if (pred[0] in counts) {
+                counts[pred[0]] += 1
+            } else {
+                counts[pred[0]] = 1
+            }
+        });
+        var seq = palette('tol-sq', 10);
+
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Object.keys(counts),
+                datasets: [{
+                    label: '# of Annotations',
+                    data: Object.values(counts),
+                    borderWidth: 1
+                }]
+            },
+            backgroundColor: palette('tol', Object.keys(counts).length).map(function(hex) {
+              console.log('#' + hex);
+              return '#' + hex;
+            }),
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
                 }
             }
-        ]
-    });
-} );
+        });
+    }
+
+});
